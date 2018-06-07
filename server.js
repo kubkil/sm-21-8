c9d97a19e1a0b01a12326bdc8012ca87bebd3ba1
Jakub Kilar <kuba.kilar@gmail.com>
2018-06-07 20:40:52 +0200

    add model template

---
+++ server.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/nodeappdatabase', {
+  useMongoClient: true
+});
+
+const userSchema = new Schema({
+  name: String,
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  admin: Boolean,
+  created_at: Date,
+  updated_at: Date
+});
+
+userSchema.methods.manify = function (next) {
+  this.name = this.name + '-boy';
+
+  return next(null, this.name);
+};
+
+userSchema.pre('save', function (next) {
+  const currentDate = new Date();
+  this.updated_at = currentDate;
+  if (!this.created_at)
+    this.created_at = currentDate;
+  next();
+});
