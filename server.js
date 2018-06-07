ee2c3c56b07f6e577d95bfe70f08ced492160035
Jakub Kilar <kuba.kilar@gmail.com>
2018-06-07 20:43:02 +0200

    create database

--- server.js
+++ server.js
@@ -28,3 +28,57 @@ userSchema.pre('save', function (next) {
     this.created_at = currentDate;
   next();
 });
+
+const User = mongoose.model('User', userSchema);
+//instancje klasy User
+const kenny = new User({
+  name: 'Kenny',
+  username: 'Kenny_the_boy',
+  password: 'password'
+});
+// TEMPLATE END
+
+kenny.manify(function (err, name) {
+  if (err) throw err;
+  console.log('Twoje nowe imię to: ' + name);
+});
+
+kenny.save(function (err) {
+  if (err) throw err;
+
+  console.log('Uzytkownik ' + kenny.name + ' zapisany pomyslnie');
+});
+
+const benny = new User({
+  name: 'Benny',
+  username: 'Benny_the_boy',
+  password: 'password'
+});
+
+benny.manify(function (err, name) {
+  if (err) throw err;
+  console.log('Twoje nowe imię to: ' + name);
+});
+
+benny.save(function (err) {
+  if (err) throw err;
+
+  console.log('Uzytkownik ' + benny.name + ' zapisany pomyslnie');
+});
+
+const mark = new User({
+  name: 'Mark',
+  username: 'Mark_the_boy',
+  password: 'password'
+});
+
+mark.manify(function (err, name) {
+  if (err) throw err;
+  console.log('Twoje nowe imię to: ' + name);
+});
+
+mark.save(function (err) {
+  if (err) throw err;
+
+  console.log('Uzytkownik ' + mark.name + ' zapisany pomyslnie');
+});
