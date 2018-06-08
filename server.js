e7aaa1662797aa96144f8df6f242e6716ad4654a
Jakub Kilar <kuba.kilar@gmail.com>
2018-06-08 21:47:04 +0200

    connect to database

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://kubkil:P0mnik89@@ds247330.mlab.com:47330/database-21', {
+mongoose.connect('mongodb://Jan:iiyama12@ds247330.mlab.com:47330/database-21', {
   useMongoClient: true
 });
 
